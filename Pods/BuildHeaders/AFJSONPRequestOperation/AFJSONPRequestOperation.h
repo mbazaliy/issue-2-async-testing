a72bed262c78274d562e85436e7e47ba6507ccf8
Max Bazaliy <mbazaliy@gmail.com>
2013-07-08 19:49:23 +0300

    Add dependencies

---
+++ Pods/BuildHeaders/AFJSONPRequestOperation/AFJSONPRequestOperation.h
@@ -0,0 +1,1 @@
+../../AFJSONPRequestOperation/AFJSONPRequestOperation.h
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFJSONPRequestOperation
+#define COCOAPODS_POD_AVAILABLE_AFJSONPRequestOperation
+#define COCOAPODS_VERSION_MAJOR_AFJSONPRequestOperation 1
+#define COCOAPODS_VERSION_MINOR_AFJSONPRequestOperation 0
+#define COCOAPODS_VERSION_PATCH_AFJSONPRequestOperation 0
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
+
